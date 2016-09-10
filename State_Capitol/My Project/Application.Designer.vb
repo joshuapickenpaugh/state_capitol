f8ffc780a1136cdfa6a6794d60f3f69763a4ee7b
joshuapickenpaugh <joshuapickenpaugh@hotmail.com>
2016-09-10 08:44:09 -0500

    First commit

--- State_Capitol/My Project/Application.Designer.vb
+++ State_Capitol/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.State_Capitol.Form1
+            Me.MainForm = Global.State_Capitol.frmMain
         End Sub
     End Class
 End Namespace
